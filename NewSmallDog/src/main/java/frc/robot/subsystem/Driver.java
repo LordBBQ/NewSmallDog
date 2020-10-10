14c4dbaf202196ff9975741ebcfdae53abcd9466
drolo776 <72638840+drolo776@users.noreply.github.com>
2020-10-10 13:49:33 +1100

    add Driver.java

---
+++ NewSmallDog/src/main/java/frc/robot/subsystem/Driver.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystem;
+
+public class Driver {
+    void calculateDrive(double speed, double direction){
+        // add joystick input into speed and direction
+
+        public double right = speed + direction;
+        public double left = speed - direction;
+
+    }
+
+    void fixDeadspot(){
+        if(power => -0.05 && power =< 0.05){
+            power = 0;
+        }
+
+        if(steering => -0.05 && steering =< 0.05){
+            steering = 0;
+        }
+        
+    }
+
+    void drive(double left, double right){
+
+        // set power, velocity
+        
+    }
+}
